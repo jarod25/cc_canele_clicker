4247f53519ea1d55f5bf893b813e2e93e23f0066
jarod25 <63459117+jarod25@users.noreply.github.com>
2020-11-07 14:56:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 <html>
 <body>
 
-<h2>What Can JavaScript Do?</h2>
-
-<p id="demo">JavaScript can change HTML content.</p>
-
-<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Vous avez un Canelé en plus"'>Cuisiner 1 canelé</button>
 
 </body>
 </html>
